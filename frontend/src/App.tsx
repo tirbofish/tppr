cc39b844fa2112a5640b7adea456b41d3afa0d72
Thribhu K <4tkbytes@pm.me>
2026-05-04 04:04:20 +0000

    feature: "created"  signup and login, learnt routing in frontend.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,17 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+// import NavBar from './components/navbar'
+import Login from './pages/Login'
 import Landing from './pages/Landing'
-import NavBar from './components/navbar'
+import Signup from './pages/Signup'
 
 function App() {
   return (
     <BrowserRouter>
-      <NavBar />
+      {/* <NavBar /> */}
       <Routes>
         <Route path="/" element={<Landing />} />
+        <Route path="/login" element={<Login />} />
+        <Route path='/signup' element={<Signup />} />
       </Routes>
     </BrowserRouter>
   )
